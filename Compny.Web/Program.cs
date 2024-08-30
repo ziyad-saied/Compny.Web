3442d36161947e505a0e2990afcdc28f7a068247
zead seam <zeadseam7@gmail.com>
2024-08-30 11:36:08 +0300

    Create New Project

--- Compny.Web/Program.cs
+++ Compny.Web/Program.cs
@@ -11,6 +11,7 @@ namespace Compny.Web
 
             var app = builder.Build();
 
+
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
